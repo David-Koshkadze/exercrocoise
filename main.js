a3ea16b07e0f33b69c518a65f469370ad1892dfc
David-Koshkadze <rootUS3rr@pm.me>
2023-03-04 11:16:08 +0400

    add styles to the pages

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 import "./style.css";
 
+const buttonStyles = "rounded py-2 px-4 bg-white bg-gradient-to-r hover:from-green-500 hover:to-green-400 transition duration-200 hover:scale-110";
+
 fetch("https://jsonplaceholder.typicode.com/users")
   .then((response) => response.json())
   .then((users) => {
-    
     const usersContainer = document.getElementById("users-container");
 
     users.forEach((user) => {
       const button = document.createElement("button");
       button.textContent = user.name;
-      
-      button.classList.add("rounded", "py-2", "px-2", "shadow-md", "bg-white")
+
+      button.classList.add(...buttonStyles.split(" "));
 
       button.addEventListener("click", () => {
         // user info page
