ebc05d1b3c9ede8aa76bbf80b77242e73b410595
David-Koshkadze <rootUS3rr@pm.me>
2023-03-04 09:41:14 +0400

    basic styles

--- main.js
+++ main.js
@@ -9,6 +9,9 @@ fetch("https://jsonplaceholder.typicode.com/users")
     users.forEach((user) => {
       const button = document.createElement("button");
       button.textContent = user.name;
+      
+      button.classList.add("rounded", "py-2", "px-2", "shadow-md", "bg-white")
+
       button.addEventListener("click", () => {
         // user info page
         window.location.href = `pages/user.html?id=${user.id}`;
